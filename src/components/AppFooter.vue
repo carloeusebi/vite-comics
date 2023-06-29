4201c60ec1212307dd07e0cef1ecf8b10919f50d
carloeusebi <carloeusebi@gmail.com>
2023-06-29 16:43:59 +0200

    Completed bottom footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,17 @@
 <script>
+import FooterBottom from './footer/FooterBottom.vue';
+import FooterJumbo from './footer/FooterJumbo.vue';
+
 export default {
+    components: { FooterBottom, FooterJumbo }
 }
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <FooterJumbo />
+        <FooterBottom />
+    </footer>
+</template>
 
-<styles>
-
-</styles>
+<style></style>
---
+++ src/components/footer/FooterJumbo.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    Jumbo
+</template>
+
+<style scoped></style>
